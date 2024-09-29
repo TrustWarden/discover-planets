a8313e6ae2650e6b4f451781acf9325f5d26ad2d
Emran Miri <11910359+Donilo@users.noreply.github.com>
2024-09-29 22:06:40 +0330

    Found habitable planets from the kepler's data

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
 
-const result = [];
+const habitalePlanets = [];
 
-const parser = parse({
-  comment: "#",
-  columns: true,
-});
+function isHabitalePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] >= 0.36 &&
+    planet["koi_insol"] <= 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
 
 fs.createReadStream("kepler_data.csv")
-  .pipe(parser)
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
-    result.push(data);
+    if (isHabitalePlanet(data)) {
+      habitalePlanets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(result);
+    console.log(habitalePlanets);
+    console.log(habitalePlanets.length);
     console.log("Done");
   });
