ee0d8f93c51708ea944e8f1f6eef22725e266237
Emran Miri <11910359+Donilo@users.noreply.github.com>
2024-10-17 00:53:22 +0330

    Installed mongoose to connect to mongoDB and created schema of launches and planets

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb://localhost:27017/discover-planets";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection is ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
