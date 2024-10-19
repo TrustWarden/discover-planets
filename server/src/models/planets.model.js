f2870c45a33fb85850bf1f9712e23ee9089a4095
Emran Miri <11910359+Donilo@users.noreply.github.com>
2024-10-19 21:04:09 +0330

    exclude the unuseful data to return from database and updated the architectural diagram

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -17,32 +17,39 @@ function loadPlanetsData() {
   return new Promise((resolve, reject) => {
     fs.createReadStream(
       path.join(__dirname, "..", "..", "data", "kepler_data.csv")
-    ).pipe(
-      parse({
-        comment: "#",
-        columns: true,
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", async (data) => {
+        if (isHabitalePlanet(data)) {
+          savePlanet(data);
+        }
       })
-        .on("data", async (data) => {
-          if (isHabitalePlanet(data)) {
-            savePlanet(data);
-          }
-        })
-        .on("error", (err) => {
-          console.log(err);
-          reject(err);
-        })
-        .on("end", async () => {
-          const countPlanetsFound = (await getAllPlanets()).length;
-          console.log(`${countPlanetsFound} habitable exoplanets founded.`);
-          console.log("Done.");
-          resolve();
-        })
-    );
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable exoplanets founded.`);
+        console.log("Done.");
+        resolve();
+      });
   });
 }
 
 async function getAllPlanets() {
-  return await planets.find({});
+  return await planets.find(
+    {},
+    {
+      _id: 0,
+      __v: 0,
+    }
+  );
 }
 
 async function savePlanet(planet) {
