479a9fa677e40f05596af37fca838bb351052b1f
Emran Miri <11910359+Donilo@users.noreply.github.com>
2024-10-22 01:31:53 +0330

    Made changes to the launches api, now totally compatible with mongodb and mongoose

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,50 +1,107 @@
-const launches = new Map();
+const launches = require("./launches.mongo");
+const planets = require("./planets.mongo");
 
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exoplanet 1",
   rocket: "Explorer 11",
-  target: "Kepler-1652 b",
+  target: "Kepler-442 b",
   customers: ["NASA", "EXOLET"],
   launchDate: new Date("May 24, 2031"),
   upcoming: true,
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
-function getAllLaunches() {
-  return Array.from(launches.values());
-}
-
-function getOneLaunch(flightNumber) {
-  return launches.get(flightNumber);
-}
-
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  launches.set(
-    latestFlightNumber,
-    Object.assign(launch, {
-      flightNumber: latestFlightNumber,
-      customers: ["NASA", "SpaceX"],
-      upcoming: true,
-      success: true,
-    })
+async function getAllLaunches() {
+  return await launches.find(
+    {},
+    {
+      _id: 0,
+      __v: 0,
+    }
   );
 }
 
-function abortLaunch(launch) {
-  launch.success = false;
-  launch.upcoming = false;
-  return launch;
+async function getOneLaunch(flightNumber) {
+  return await launches.findOne(
+    {
+      flightNumber: flightNumber,
+    },
+    {
+      _id: 0,
+      __v: 0,
+    }
+  );
+}
+
+async function getLatestFlightNumber() {
+  const latestLaunch = await launches.findOne().sort("-flightNumber");
+
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+
+  return latestLaunch.flightNumber;
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
+
+  try {
+    await launches.findOneAndUpdate(
+      {
+        flightNumber: launch.flightNumber,
+      },
+      launch,
+      {
+        upsert: true,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = (await getLatestFlightNumber()) + 1;
+
+  const fullLaunch = Object.assign(launch, {
+    flightNumber: newFlightNumber,
+    customers: ["NASA", "SpaceX"],
+    upcoming: true,
+    success: true,
+  });
+
+  await saveLaunch(fullLaunch);
+}
+
+async function abortLaunch(launch) {
+  const aborted = await launches.updateOne(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
   getAllLaunches,
   getOneLaunch,
-  addNewLaunch,
+  scheduleNewLaunch,
   abortLaunch,
 };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,20 +1,20 @@
 const {
   getAllLaunches,
   getOneLaunch,
-  addNewLaunch,
+  scheduleNewLaunch,
   abortLaunch,
 } = require("../../models/launches.model");
 
-function httpGetAllLaunches(req, res) {
-  return res.status(200).json(getAllLaunches());
+async function httpGetAllLaunches(req, res) {
+  return res.status(200).json(await getAllLaunches());
 }
 
-function httpGetOneLaunch(req, res) {
+async function httpGetOneLaunch(req, res) {
   const flightNumber = Number(req.params.id);
-  return res.status(200).json(getOneLaunch(flightNumber));
+  return res.status(200).json(await getOneLaunch(flightNumber));
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
   const launch = req.body;
   if (
     !launch.mission ||
@@ -34,18 +34,30 @@ function httpAddNewLaunch(req, res) {
     });
   }
 
-  addNewLaunch(launch);
+  await scheduleNewLaunch(launch);
+  console.log(launch);
   return res.status(201).json(launch);
 }
 
-function httpAbortLaunch(req, res) {
+async function httpAbortLaunch(req, res) {
   const flightNumber = Number(req.params.id);
 
-  const launch = getOneLaunch(flightNumber);
+  const launch = await getOneLaunch(flightNumber);
+
+  console.log(launch);
 
   if (launch) {
-    const abortedLaunch = abortLaunch(launch);
-    return res.status(200).json(abortedLaunch);
+    const abortedLaunch = await abortLaunch(launch);
+
+    if (!abortedLaunch) {
+      return res.status(400).json({
+        error: "Launch not aborted.",
+      });
+    }
+
+    return res.status(200).json({
+      ok: 1,
+    });
   } else {
     return res.status(404).json({
       error: "The flight not found.",
